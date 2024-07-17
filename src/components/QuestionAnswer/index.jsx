3efcf10c18c72818129a222b7fc84291161330c3
charlesalmeiida <charlesalmeida844@gmail.com>
2024-07-17 20:43:54 -0300

    feat: create state of answers

--- src/components/QuestionAnswer/index.jsx
+++ src/components/QuestionAnswer/index.jsx
@@ -1,5 +1,14 @@
 import S from "./styles.module.css"
 
-export function QuestionAnswer() {
-  return <button className={S.container}>Test 1</button>
+export function QuestionAnswer(props) {
+  return (
+    <button
+      onClick={(event) =>
+        props.handleAnswerQuestion(event, props.question, props.answer)
+      }
+      className={S.container}
+    >
+      {props.answer}
+    </button>
+  )
 }
--- src/components/quiz/index.jsx
+++ src/components/quiz/index.jsx
@@ -1,28 +1,78 @@
+import { useState } from "react"
 import { QuestionAnswer } from "../QuestionAnswer"
 import S from "./styles.module.css"
 
+const QUESTIONS = [
+  {
+    id: 1,
+    question: "Qual é o meu nome?",
+    answers: ["Matheus", "Charles", "Carlos", "Tiago"],
+    correctAnswer: "Charles",
+  },
+  {
+    id: 2,
+    question: "Qual é a minha idade?",
+    answers: ["12", "30", "27", "19"],
+    correctAnswer: "27",
+  },
+  {
+    id: 3,
+    question: "Qual é a minha profissão?",
+    answers: ["Desenvolvedor", "Padeiro", "Designer", "Arquiteto"],
+    correctAnswer: "Desenvolvedor",
+  },
+  {
+    id: 4,
+    question: "Quem é Tony Stark?",
+    answers: [
+      "Homem-aranha",
+      "Homem de Ferro",
+      "Capitão América",
+      "Soldado Invernal",
+    ],
+    correctAnswer: "Homem de Ferro",
+  },
+]
+
 export function Quiz() {
+  const currentQuestion = QUESTIONS[0]
+  const [count, setCount] = useState(0)
+  const [isCurrentQuestionAswered, setCurrentQuestionAnswered] = useState(false)
+
+  function handleAnswerQuestion(event, question, answer) {
+    if (isCurrentQuestionAswered) {
+      return
+    }
+
+    const isCorrectAnswer = question.correctAnswer === answer
+    const resultClassName = isCorrectAnswer ? S.correct : S.incorrect
+    event.currentTarget.classList.toggle(resultClassName)
+
+    if (isCorrectAnswer) {
+      setCount(count + 1)
+    }
+
+    setCurrentQuestionAnswered(true)
+  }
+
   return (
     <div className={S.container}>
       <div className={S.card}>
         <div className={S.quiz}>
           <header className={S.quizHeader}>
             <span className={S.questionCount}>PERGUNTA 1/3</span>
-            <p className={S.question}>Qual é o meu nome?</p>
+            <p className={S.question}>{currentQuestion.question}</p>
           </header>
           <ul className={S.answers}>
-            <li>
-              <QuestionAnswer />
-            </li>
-            <li>
-              <QuestionAnswer />
-            </li>
-            <li>
-              <QuestionAnswer />
-            </li>
-            <li>
-              <QuestionAnswer />
-            </li>
+            {currentQuestion.answers.map((answer) => (
+              <li key={answer.id}>
+                <QuestionAnswer
+                  answer={answer}
+                  question={currentQuestion}
+                  handleAnswerQuestion={handleAnswerQuestion}
+                />
+              </li>
+            ))}
           </ul>
         </div>
       </div>
